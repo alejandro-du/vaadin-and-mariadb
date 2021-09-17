1de5bd6f37ec6e47734bddfb0f9cfb7c562f639a
Alejandro Duarte <alejandro@vaadin.com>
2021-09-17 14:41:34 +0300

    Starting point

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,21 +1,18 @@
 package com.example.application;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
-
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.vaadin.artur.helpers.LaunchUtil;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.component.dependency.NpmPackage;
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "demo")
@@ -24,7 +21,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
-        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        SpringApplication.run(Application.class, args);
     }
 
 }
